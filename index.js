140b9bbfe678482022ecaab5c4c5fd56d7d3d26d
Marc vd Made <0996549@hr.nl>
2021-09-06 15:40:28 +0200

    following vue docs conditionals and loops

---
+++ index.js
@@ -0,0 +1,31 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!',
+    }
+})
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'Your page loaded on ' + new Date().toLocaleString(),
+    }
+})
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+})
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: 'Learn Javascript' },
+            { text: 'Learn Vue'},
+            { text: 'Build something awesome' }
+        ]
+    }
+})
